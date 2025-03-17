7a7cb1f982e15c49ff4301e54e5ddb0ec3af9df1
RajThakur777 <traj15019@gmail.com>
2025-03-17 23:55:44 +0530

    Initial commit

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,12 @@
+import Youtube from "./Youtube";
+
+function App() {
+  return (
+    <>
+    <h1>My App</h1>
+    <Youtube />
+    </>
+  );
+}
+
+export default App;
---
+++ 01basicvite/src/main.jsx
@@ -0,0 +1,35 @@
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+import React from 'react'
+
+const reactElement = {
+    type: 'a',
+    props: {
+      href: "https://google.com",
+      target: '_blank'
+    },
+    children: 'Click me to visit google'
+  }
+
+function MyApp() {
+    return (
+        <div>
+            <h1>Custome React app</h1>
+        </div>
+    )
+}
+
+const AnotherElement = (
+    <a href="http://google.com" target='_blank'>Visit Google</a>
+)
+
+const areactElement = React.createElement(
+    'a',
+    {href: 'https://google.com' , target: "_blank"}, 'click to visit google'
+)
+
+createRoot(document.getElementById('root')).render(
+    <App/>
+    reactElement
+    
+)
